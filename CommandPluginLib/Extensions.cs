cfbcc6d2a099c08376679756123b2acd02c54c54
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-03-04 03:11:46 -0600

    Initial upload

---
+++ CommandPluginLib/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommandPluginLib;
+using Newtonsoft.Json;
+
+namespace CommandPluginLib
+{
+    public static class Extensions
+    {
+        public static string ToJSON(this MessageData s)
+        {
+            return JsonConvert.SerializeObject(s);
+        }
+
+        public static MessageData ToMessageData(this string d)
+        {
+            return JsonConvert.DeserializeObject<MessageData>(d);
+        }
+    }
+}
---
+++ CommandPluginLib/ICommandPlugin.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+
+namespace CommandPluginLib
+{
+    public interface ICommandPlugin
+    {
+        /// <summary>
+        /// Name used to identify the plugin. Messages are prefixed with this to identify the plugin as the source/destination.
+        /// </summary>
+        string PluginName { get; }
+
+        /// <summary>
+        /// List of commands that can be received by this plugin.
+        /// </summary>
+        List<string> Commands { get; }
+
+        /// <summary>
+        /// Code to execute when a message is received.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void OnMessage(object sender, MessageData e);
+
+        /// <summary>
+        /// Executed once the plugin is loaded.
+        /// </summary>
+        void Start();
+
+        /// <summary>
+        /// Messages to send to the server.
+        /// </summary>
+        event Action<MessageData> MessageReady;
+    }
+
+    /// <summary>
+    /// An imitation of WebSocketSharp's MessageEventArgs class to avoid plugins needing to reference it.
+    /// </summary>
+    public struct MessageData
+    {
+        public string Source;
+        public string Destination;
+        public string Data;
+        public string Flag;
+
+        /// <summary>
+        /// Constructor for MessageData.
+        /// </summary>
+        /// <param name="_src">Source of the message</param>
+        /// <param name="_dest">Destination of the message</param>
+        /// <param name="_data">Message data</param>
+        public MessageData(string _src, string _dest, string _data, string _flag ="")
+        {
+            Console.WriteLine("Constructed new message!");
+            Source = _src;
+            Destination = _dest;
+            Data = _data;
+            Flag = _flag;
+        }
+
+    }
+}
