42b08309647f70fe98bc224ce99c14fd705dc512
Zingabopp <37518403+Zingabopp@users.noreply.github.com>
2019-03-08 01:13:01 -0600

    Added AddSafe Dictionary extension. Documentation on extensions.

--- CommandPluginLib/Extensions.cs
+++ CommandPluginLib/Extensions.cs
@@ -10,6 +10,11 @@ namespace CommandPluginLib
 {
     public static class Extensions
     {
+        /// <summary>
+        /// Serializes a MessageData into a JSON string.
+        /// </summary>
+        /// <param name="s">MessageData to serialize.</param>
+        /// <returns>JSON string from a serialized MessageData.</returns>
         public static string ToJSON(this MessageData s)
         {
             try
@@ -23,6 +28,11 @@ namespace CommandPluginLib
             }
         }
 
+        /// <summary>
+        /// Deserializes a JSON string to a MessageData struct.
+        /// </summary>
+        /// <param name="d">MessageData serialized as a JSON string</param>
+        /// <returns>A MessageData with the values from the JSON string.</returns>
         public static MessageData ToMessageData(this string d)
         {
             try
@@ -35,6 +45,26 @@ namespace CommandPluginLib
                 throw ex;
             }
             
+        }
+
+        /// <summary>
+        /// Attempts to add a Key/Value pair to a Dictionary. If the Key already exists and update is true, the value is changed to the provided value.
+        /// </summary>
+        /// <typeparam name="TKey"></typeparam>
+        /// <typeparam name="TValue"></typeparam>
+        /// <param name="source"></param>
+        /// <param name="key"></param>
+        /// <param name="value">Value to add/update</param>
+        /// <param name="update">If true, updates the existing key's value.</param>
+        public static void AddSafe<TKey, TValue>(this IDictionary<TKey, TValue> source, TKey key, TValue value, bool update = true)
+        {
+            if (!source.ContainsKey(key))
+                source.Add(key, value);
+            else
+            {
+                if (update)
+                    source[key] = value;
+            }
         }
     }
 }
